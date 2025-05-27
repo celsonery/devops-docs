4f2c4537e276705549b0aabe8c0bf2f840e7b21d
Celso Nery <celso.nery@gmail.com>
2025-05-27 14:11:48 -0300

    chore: Add comment in bug_report.yml, config.yml and feature_report.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,102 +43,102 @@ export default defineUserConfig({
       },
       ],
     sidebar: [
-    {
-      text: 'Docker',
-      collapsible: true,
-      children: [
-          '/docker/docker',
-          '/docker/install-docker',
-          '/docker/volumes-docker'
+      {
+        text: 'Docker',
+        collapsible: true,
+        children: [
+            '/docker/docker',
+            '/docker/install-docker',
+            '/docker/volumes-docker'
+          ],
+      },
+      {
+        text: 'kubernetes',
+        collapsible: true,
+        children: [
+          '/kubernetes/prepare',
+          '/kubernetes/install-kubernetes',
+          '/kubernetes/initialize-master',
+          '/kubernetes/initialize-slave',
+          '/kubernetes/running-yamls',
+          '/kubernetes/running',
+          '/kubernetes/server-registry-local',
+          '/kubernetes/helm',
+          '/kubernetes/metric-server',
+          '/kubernetes/volumes-k8s',
+          '/kubernetes/renovar-certs',
+          '/kubernetes/add-new-node-at-kubernetes',
+          '/kubernetes/regcred',
+          '/kubernetes/commands',
+          '/kubernetes/service-yaml',
+          '/kubernetes/using',
+          ],
+      },
+      {
+        text: 'kubernetes-addons',
+        collapsible: true,
+        children: [
+          '/kubernetes-addons/cert-manager',
+          '/kubernetes-addons/ingress-controller',
+          '/kubernetes-addons/prometheus',
+          '/kubernetes-addons/grafana',
+          '/kubernetes-addons/portainer',
+          '/kubernetes-addons/redis-cluster',
         ],
-    },
-    {
-      text: 'kubernetes',
-      collapsible: true,
-      children: [
-        '/kubernetes/prepare',
-        '/kubernetes/install-kubernetes',
-        '/kubernetes/initialize-master',
-        '/kubernetes/initialize-slave',
-        '/kubernetes/running-yamls',
-        '/kubernetes/running',
-        '/kubernetes/server-registry-local',
-        '/kubernetes/helm',
-        '/kubernetes/metric-server',
-        '/kubernetes/volumes-k8s',
-        '/kubernetes/renovar-certs',
-        '/kubernetes/add-new-node-at-kubernetes',
-        '/kubernetes/regcred',
-        '/kubernetes/commands',
-        '/kubernetes/service-yaml',
-        '/kubernetes/using',
+      },
+      {
+        text: 'kvm',
+        collapsible: true,
+        children: [
+          '/kvm/disk-resize-kvm'
         ],
-    },
-    {
-      text: 'kubernetes-addons',
-      collapsible: true,
-      children: [
-        '/kubernetes-addons/cert-manager',
-        '/kubernetes-addons/ingress-controller',
-        '/kubernetes-addons/prometheus',
-        '/kubernetes-addons/grafana',
-        '/kubernetes-addons/portainer',
-        '/kubernetes-addons/redis-cluster',
+      },
+      {
+        text: 'gitlab',
+        collapsible: true,
+        children: [
+          '/gitlab/gitlab'
+        ],
+      },
+      {
+        text: 'keycloak',
+        collapsible: true,
+        children: [
+          '/keycloak/keycloak-15',
+          '/keycloak/keycloak-26',
+        ],
+      },
+      {
+        text: 'postgres',
+        collapsible: true,
+        children: [
+          '/postgres/postgres',
+          '/postgres/zabbix-postgres',
+        ],
+      },
+      {
+        text: 'letsencrypt',
+        collapsible: true,
+        children: [
+          '/letsencrypt/letsencrypt',
+        ],
+      },
+      {
+        text: 'vsftp',
+        collapsible: true,
+        children: [
+          '/vsftp/vsftp',
+        ],
+      },
+      {
+        text: 'Others',
+        collapsible: true,
+        children: [
+          '/others/k3s',
+          '/others/install-minikube',
+          '/others/install-openshift'
+        ],
+      }
       ],
-    },
-    {
-      text: 'kvm',
-      collapsible: true,
-      children: [
-        '/kvm/disk-resize-kvm'
-      ],
-    },
-    {
-      text: 'gitlab',
-      collapsible: true,
-      children: [
-        '/gitlab/gitlab'
-      ],
-    },
-    {
-      text: 'keycloak',
-      collapsible: true,
-      children: [
-        '/keycloak/keycloak-15',
-        '/keycloak/keycloak-26',
-      ],
-    },
-    {
-      text: 'postgres',
-      collapsible: true,
-      children: [
-        '/postgres/postgres',
-        '/postgres/zabbix-postgres',
-      ],
-    },
-    {
-      text: 'letsencrypt',
-      collapsible: true,
-      children: [
-        '/letsencrypt/letsencrypt',
-      ],
-    },
-    {
-      text: 'vsftp',
-      collapsible: true,
-      children: [
-        '/vsftp/vsftp',
-      ],
-    },
-    {
-      text: 'Others',
-      collapsible: true,
-      children: [
-        '/others/k3s',
-        '/others/install-minikube',
-        '/others/install-openshift'
-      ],
-    }
-    ],
   }),
 })
